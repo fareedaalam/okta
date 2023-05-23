1a61251fc150123b40fd5bda7aed28005aa90ab5
fareedaalam <fareedaalam.1512@gmail.com>
2023-05-23 13:59:07 +0530

    Update OktaAuthConfigExtentions.cs

--- TodoApi/Extentions/OktaAuthConfigExtentions.cs
+++ TodoApi/Extentions/OktaAuthConfigExtentions.cs
@@ -16,17 +16,37 @@ namespace TodoApi.Extentions
              services.Configure<OktaTokenSettings>(config.GetSection("Okta"));           
             var oktaSettings = config.GetSection("Okta").Get<OktaTokenSettings>();
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.Audience = oktaSettings.Audience;
+                  options.Authority = oktaSettings.Issuer;
+                  
+              });
+
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequiredAdminRole", policy =>
+                    policy.RequireClaim("groups","MeetAdmin"));
+                opt.AddPolicy("RequiredUserRole", policy => 
+                    policy.RequireClaim("groups", "MeetUser"));
+
+            });
+            /***
             services.AddAuthentication(option =>
             {
-                option.DefaultAuthenticateScheme = OktaDefaults.ApiAuthenticationScheme;
-                option.DefaultChallengeScheme = OktaDefaults.ApiAuthenticationScheme;
-                option.DefaultSignInScheme = OktaDefaults.ApiAuthenticationScheme;
+                     options.Audience = oktaSettings.Audience;
+                  options.Authority = oktaSettings.Issuer;
+               // option.DefaultAuthenticateScheme = OktaDefaults.ApiAuthenticationScheme;
+               // option.DefaultChallengeScheme = OktaDefaults.ApiAuthenticationScheme;
+               // option.DefaultSignInScheme = OktaDefaults.ApiAuthenticationScheme;
             }).AddOktaWebApi(new OktaWebApiOptions
             {
                 OktaDomain = oktaSettings.Domain,
                 AuthorizationServerId = oktaSettings.AuthorizationServerId,
                 Audience = oktaSettings.Audience
             });
+            ***/
 
             return services;
         }
